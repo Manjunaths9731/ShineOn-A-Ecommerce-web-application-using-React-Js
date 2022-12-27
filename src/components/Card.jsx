9aa270b1acca12035a2bd59c7c9ec8616e64782a
Manjunaths9731 <66406975+Manjunaths9731@users.noreply.github.com>
2022-12-27 17:12:16 +0530

    Update Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -17,11 +17,6 @@ const Card = ({ flag, data, scrollValue }) => {
     });
     localStorage.setItem("cartItems", JSON.stringify(items));
   };
-
-  useEffect(() => {
-    addtocart();
-  }, [items]);
-
   return (
     <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-5 gap-2  lg:gap-3 md:gap-3">
       {mainData &&
