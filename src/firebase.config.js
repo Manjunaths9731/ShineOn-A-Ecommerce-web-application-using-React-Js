6c3d304543ae5f985663c1c83c47838095e3179b
Manjunaths9731 <66406975+Manjunaths9731@users.noreply.github.com>
2023-08-26 14:43:20 +0530

    Update firebase.config.js

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -3,13 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL:"",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
+  apiKey: "AIzaSyBVR7biG08ykPnWQtFhbs7Stuo1jr-qzMc",
+  authDomain: "shineon-400ce.firebaseapp.com",
+  databaseURL:"https://shineon-400ce-default-rtdb.firebaseio.com",
+  projectId: "shineon-400ce",
+  storageBucket: "shineon-400ce.appspot.com",
+  messagingSenderId: "201908110476",
+  appId: "1:201908110476:web:816ca7397722fe8404e7a9",
 };
 
 const app = getApps.length >0 ? getApp() : initializeApp(firebaseConfig);
